ec202c9c727e56533a926877b9b097a75fd449d9
Niklas Semmler <niklas.semmler@mailbox.org>
2023-10-09 11:39:34 +0200

    formatting

---
+++ vim/lua/fubar.lua
@@ -0,0 +1,3 @@
+local ut = require("null-ls.utils")
+local m = ut.root_pattern(".null-ls-root", "Makefile", ".git")
+print(m("/Users/nsemmler/Documents/snowflake/Tests/MakeBenchmark2/src/make_benchmark2/secret_util.py"))
